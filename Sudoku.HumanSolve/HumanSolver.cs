07c5ecc19af2120b49591abdd9979087f6e5846a
Nico Buff <nicolas.buffenoir@gmail.com>
2024-11-29 20:34:51 +0100

    Projet créé avec référence

---
+++ Sudoku.HumanSolve/HumanSolver.cs
@@ -0,0 +1,12 @@
+﻿using Sudoku.Shared;
+
+namespace Sudoku.HumanSolve;
+
+public class EmptySolver : ISudokuSolver
+    {
+        public SudokuGrid Solve(SudokuGrid s)
+        {
+            return s.CloneSudoku();
+        }
+
+    }
